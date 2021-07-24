17112db708108a9fd0811984c1ea308615ab5438
Akshat94-max <76423504+Akshat94-max@users.noreply.github.com>
2021-07-24 15:30:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -34,14 +34,14 @@ function draw() {
 
     if(block1.isTouching(ball) && ball.bounceOff(block1)){
         ball.shapeColor = rgb(0,0,255);
-        music.play();
+      
     }
 
     if(block2.isTouching(ball)){
         ball.shapeColor = rgb(255,128,0);
         ball.velocityX = 0;
         ball.velocityY = 0;
-        music.stop();
+    
     }
 
     if(block3.isTouching(ball) && ball.bounceOff(block3)){
